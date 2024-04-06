e9e7586fd8bfc5d67c552de991498388ab83f629
Deepak kushwah <90465100+Deepakkushwahg@users.noreply.github.com>
2024-04-06 14:29:00 +0530

    first commit

---
+++ Frontend/src/Components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './/NavBar.js'
+import Sectionfirst from './Sectionfirst.js'
+import Sectionsecond from './Sectionsecond.js'
+import Sectionthird from './Sectionthird.js'
+
+
+export default function HomePage() {
+  return (
+    
+        <div><Sectionfirst/>
+        <Sectionsecond/>
+        <Sectionthird/>
+        </div>
+       
+  )
+}
---
+++ Frontend/src/Components/Sectionfirst.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../Constants/Css/Sectionfirst.css";
+import { IoEnterOutline } from "react-icons/io5";
+
+export default function Sectionfirst() {
+  return (
+    <div id="firstsection">
+        <div id="learn">
+          <button><a href="/about">LEARN MORE</a></button>
+        </div>
+      </div>
+
+    );
+}
